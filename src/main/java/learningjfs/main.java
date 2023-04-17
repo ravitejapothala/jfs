7e55b721edf0d2a9ea29c344bf9fc2a3360d5b05
ravitejapothala <raviteja.pothala.753@my.csun.edu>
2023-04-17 15:12:48 -0700

    intellij practice added for loop

--- src/main/java/learningjfs/main.java
+++ src/main/java/learningjfs/main.java
@@ -3,5 +3,9 @@ package learningjfs;
 public class main {
     public static void main(String[] args) {
         System.out.println("start practice");
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("5 times hello world");
+        }
     }
 }
